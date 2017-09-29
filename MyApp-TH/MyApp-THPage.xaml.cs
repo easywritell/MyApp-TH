2332d6d63c4a07cf4308bde2be17203302017d9c
easywritell <easywritell@gmail.com>
2017-09-29 12:22:49 +0700

    1

--- MyApp-TH/MyApp-THPage.xaml.cs
+++ MyApp-TH/MyApp-THPage.xaml.cs
@@ -7,6 +7,24 @@ namespace MyAppTH
         public MyApp_THPage()
         {
             InitializeComponent();
+
+
+            genderPicker.Items.Add("เพศชาย");
+            genderPicker.Items.Add("เพศหญิง");
+
+            salarySlider.ValueChanged += SalarySlider_ValueChanged;
+            childentStepper.ValueChanged += ChildentStepper_ValueChanged;
+        }
+
+        void SalarySlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            int value = (int)e.NewValue;
+
+        }
+
+        void ChildentStepper_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            
         }
     }
 }
